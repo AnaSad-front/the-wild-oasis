066cb1e166c8e36056b1ed1ca2c6b76711ac04f8
AnaSad-front <anastasiasadyreva1@gmail.com>
2025-06-23 18:22:52 +0300

    Uploading images to supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ozypwcjazsftxptuudhg.supabase.co";
+export const supabaseUrl = "https://ozypwcjazsftxptuudhg.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96eXB3Y2phenNmdHhwdHV1ZGhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyNTQyNzQsImV4cCI6MjA2NTgzMDI3NH0.DGA3wLXfk1SbpvXyNw2Y_W-s7S8fZq5jBMCM3w2PhUY";
 const supabase = createClient(supabaseUrl, supabaseKey);
